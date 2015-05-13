f31857d9e0069e17b61d6f0f3e061704c85d7b37
VladikLesko <vladik-lesko@ro.ru>
2015-05-13 17:51:02 +0300

    image

--- app3.py
+++ app3.py
@@ -1,7 +1,10 @@
+#qpy:kivy
 from kivy import require
 require("1.8.0")
 
 from kivy.app import App
+
+from kivy.clock import Clock
 
 from kivy.uix.button import Button
 from kivy.uix.screenmanager import Screen, ScreenManager
@@ -17,6 +20,21 @@ Builder.load_string("""
 <MainButtonClass>:
 	bold: True
 	font_size: 30
+
+<LoadingWindow>:
+	name: "LoadingWindow"
+	FloatLayout:
+		Label:
+			font_size: 35
+			text: "Loading..."
+			size_hint: (0.25, 0.1)
+			pos_hint: {"x": 0.01, "y": 0.01}
+			background_color: (0, 0, 0, 0)
+		Image:
+			size_hint: (0.2, 0.2)
+			pos_hint: {"x": 0.79, "y": 0.01}
+			source: "textures/loadingimage.png"
+
 <MainWindow>:
 	name: "MainWindow"
 	FloatLayout:
@@ -36,13 +54,18 @@ Builder.load_string("""
 			on_press:
 				root.manager.transition.direction = root.get_random_direction()	
 				root.manager.current = "SettingsWindow"
+
 <SettingsWindow>:
 	name: "SettingsWindow"
-	MainButtonClass:
-		background_color: (1, 0, 0, 1)
-		on_press:
-			root.manager.transition.direction = root.get_random_direction()
-			root.manager.current = "MainWindow"
+	FloatLayout:
+		MainButtonClass:
+			text: "Return"
+			size_hint: (0.25, 0.1)
+			pos_hint: {"x": 0.7, "y": 0.05}
+			background_color: (0, 0, 0, 0)
+			on_press:
+				root.manager.transition.direction = root.get_random_direction()
+				root.manager.current = "MainWindow"
 """)
 
 class MainButtonClass(Button) :
@@ -59,6 +82,9 @@ class MainScreenClass(Screen) :
 		lastDirection = newDirection
 		return(newDirection)
 
+class LoadingWindow(MainScreenClass) :
+	pass
+
 class MainWindow(MainScreenClass) :
 	pass
 
@@ -66,12 +92,19 @@ class SettingsWindow(MainScreenClass) :
 	pass
 
 MainScreenManager = ScreenManager()
+MainScreenManager.add_widget(LoadingWindow())
 MainScreenManager.add_widget(MainWindow())
 MainScreenManager.add_widget(SettingsWindow())
+MainScreenManager.current = "LoadingWindow"
 
-class MainApp(App) :
+def SetMainScreen(dt) :
+	MainScreenManager.current = "MainWindow"
+
+Clock.schedule_once(SetMainScreen, 2.5)
+
+class GameApp(App) :
 	def build(self) :
 		return(MainScreenManager)
 
 if(__name__ == "__main__") :
-	MainApp().run()
+	GameApp().run()
